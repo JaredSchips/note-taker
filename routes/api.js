9261280e9f1895e1c672febdfd9f07c6debdb7c8
JaredSchips <jared.schips@gmail.com>
2022-12-13 15:20:21 -0500

    Add utilities and api POST notes

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,36 @@
 const api = require('express').Router()
-const db = require('../db/db.json')
+const uuid = require('uuid')['v4']
+const fs = require('fs')
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils')
 
 // GET route for getting the whole db.json
 api.get('/notes', (req, res) => {
-    res.json(db)
+    readFromFile('./db/db.json').then(db => 
+        res.json(JSON.parse(db))
+    )
+})
+
+// POST route for adding to db.json
+api.post('/notes', (req, res) => {
+    const body = req.body
+    if (!body) {
+        return res.status(400).send('Bad Request: body is empty')
+    }
+    if (typeof body !== 'object') {
+        return res.status(400).send('Bad Request: body is not an object')
+    }
+    if (!body.title) {
+        return res.status(400).send('Bad Request: body doesn\'t contain title')
+    }
+    if (!body.text) {
+        return res.status(400).send('Bad Request: body doesn\'t contain text')
+    }
+
+    body['id'] = uuid()
+
+    readAndAppend(body, './db/db.json')
+    
+    res.status(200).json(body)
 })
 
 module.exports = api
